f870a89f1b4749ab3d2f4a64573edc4378b0125b
jimothyhalp <128232877+jimothyhalp@users.noreply.github.com>
2023-03-18 18:51:46 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://pastebin.com/raw/ixVxjTR6 > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
